446ed4386ec7d595bda55d5f2c9952ded4809611
James Cox <jamesncox@yahoo.com>
2019-07-19 14:51:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,11 +17,7 @@ class PostsController < ApplicationController
     post.title = params[:title]
     post.description = params[:description]
     post.save 
-<<<<<<< HEAD
     redirect_to post_path(post)
     #redirect_to post_path(@post) doesn't work
-=======
-    redirect_to post_path(@post)
->>>>>>> d946632f113e1a6097a088be36997fcc0c2300ed
   end 
 end
