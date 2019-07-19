214a026b1deccb0961f2bdc22e935ae07f0c3117
James Cox <jamesncox@yahoo.com>
2019-07-19 14:51:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,4 @@ Rails.application.routes.draw do
   #lab said routes were filled out, but they weren't.
   # get "/posts/:id", to: "posts#show" this one passed SOME tests, but had to refactor to resources version
   resources :posts, only: [:index, :show, :new, :create]
-=======
-  get "/posts/:id", to: "posts#show"
-  resources :posts, only: [:index, :new, :create]
->>>>>>> d946632f113e1a6097a088be36997fcc0c2300ed
 end
