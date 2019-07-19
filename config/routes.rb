a50b6a59cf2d9195b97e56618118d8f9877431df
James Cox <jamesncox@yahoo.com>
2019-07-19 14:51:49 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
   
   #lab said routes were filled out, but they weren't.
   # get "/posts/:id", to: "posts#show" this one passed SOME tests, but had to refactor to resources version
